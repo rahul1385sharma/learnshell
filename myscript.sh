3152dabd1d2d145994edaa0df92d380fcf229c1d
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-01 21:45:18 -0500

    commit

--- myscript.sh
+++ myscript.sh
@@ -3,24 +3,7 @@ source common.sh
 #foo
 #myadd
 #
-#if [[ -z "$ttt" ]]; then
-#  echo missing
-#  exit
-#fi
-#  echo $ttt
-ttt=$1
-
-#if [ -n "${ttt}" ]; then
-#  echo ${ttt}
-#  echo exiting
-#  exit
-#fi
-
-if [ -z "${ttt}" ]; then
-  echo lhs missing
+if [ -z "${pswd}" -a -z "$1" ]; then
+  echo Input password is missing
+  exit
 fi
-
-if [ -z "$1" ]; then
-  echo rhs missing
-fi
-  echo $ttt
