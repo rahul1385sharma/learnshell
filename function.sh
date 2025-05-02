6c68a083c18cfd3470b3eb2a070c1385b5ac5075
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-01 20:35:51 -0500

    commit

--- function.sh
+++ function.sh
@@ -1,25 +1,12 @@
-add() {
-  # my first function
-  x=10
-  y=10
-  z=$((x+y))
-
-  echo x -$x
-  echo y - $y
-  echo z -$z
-}
-
 multiply() {
-
   a=$1
   b=$2
  # c=$((a*b))
-
   echo a - $a
   echo b - $b
  # echo Multiply of a*b is ${c}
-
 }
-add
+
 multiply
+
 
