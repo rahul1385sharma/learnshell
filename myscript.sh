aaff5f3cdd0f5b5b3eaec0167d3139c28bcd983f
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-03 19:55:24 -0500

    commit

--- myscript.sh
+++ myscript.sh
@@ -5,15 +5,22 @@ source common.sh
 #foo
 #myadd
 #
-pswd=$1
-if [ -z "${pswd}" ]; then
-  echo Input password is missing
-  exit
-fi
-#
-if [ -n "${pswd}" ]; then
-  PASSWORD=$pswd
-  echo password - $PASSWORD
+#pswd=$1
+#if [ -z "${pswd}" ]; then
+#  echo Input password is missing
+#  exit
+#fi
+##
+#if [ -n "${pswd}" ]; then
+#  PASSWORD=$pswd
+#  echo password - $PASSWORD
+#fi
+
+r=$1
+if [ r -le 5]; then
+  echo r is less than equal to 5 - $r
+else
+  echo r is greater than 5 - $r
 fi
 #
 echo pswd -  $pswd
