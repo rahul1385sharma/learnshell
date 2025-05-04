b03053fe4fb4cb28d625fe268a0d722f26ae9fa1
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-03 21:16:15 -0500

    commit

--- loops.sh
+++ loops.sh
@@ -1,10 +1,22 @@
 # looping concepts
+#https://www.geeksforgeeks.org/looping-statements-shell-script/
 
-a=0
-while [ $a -lt $1 ]
+#a=0
+#while [ $a -lt $1 ]
+#do
+#    # Print the values
+#    echo $a
+#    # increment the value
+#    a=`expr $a + 1`
+#done
+
+
+for a in 1 2 3 4 5 6 7 8 9 10
 do
-    # Print the values
-    echo $a
-    # increment the value
-    a=`expr $a + 1`
+
+    if [ $a == 5 ]
+    then
+        break
+    fi
+    echo Iteration no $a
 done
