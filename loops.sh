9cfaff807e4d8e5176a6e04558de9c77aeddba9d
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-03 21:27:59 -0500

    commit

--- loops.sh
+++ loops.sh
@@ -21,10 +21,30 @@
 #    echo Iteration no $a
 #done
 
-for i in {1..20}; do
-    if [ "$i" -eq 10 ]; then
-        echo "Skipping $i"
-        continue
-    fi
-    echo "Number: $i"
+#for i in {1..20}; do
+#    if [ "$i" -eq 10 ]; then
+#        echo "Skipping $i"
+#        continue
+#    fi
+#    echo "Number: $i"
+#done
+
+a=0
+
+
+# -gt is greater than operator
+#Iterate the loop until a is greater than 10
+
+
+until [ $a -gt 10 ]
+do
+
+
+    # Print the values
+    echo $a
+
+
+    # increment the value
+    a=`expr $a + 1`
 done
+
