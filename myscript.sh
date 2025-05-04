f9a378e80713635670ff9ea6868d0cf7bd1a2e92
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-03 20:09:13 -0500

    commit

--- myscript.sh
+++ myscript.sh
@@ -16,14 +16,21 @@ source common.sh
 #  echo password - $PASSWORD
 #fi
 
-r=$1
-if [ "$r" -le 50 ]; then
-  echo r is less than equal to 50 - $r
-elif [ "$r" -le 100 ]; then
-  echo r is less than or equal to 100 - $r
-
-else
- echo r is greater than 100 -$r
-fi
+#r=$1
+#if [ "$r" -le 50 ]; then
+#  echo r is less than equal to 50 - $r
+#elif [ "$r" -le 100 ]; then
+#  echo r is less than or equal to 100 - $r
 #
-echo pswd -  $pswd
+#else
+# echo r is greater than 100 -$r
+#fi
+##
+#echo pswd -  $pswd
+
+echo name - ${name}
+if [ "$name" == rahul]; then
+  echo hi rahul
+else
+echo you are not rahul -$name
+fi
