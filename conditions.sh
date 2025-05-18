440e71e429b12c5cecfccb3434fef6b277c59eb6
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-05-17 19:03:08 -0500

    commit

---
+++ conditions.sh
@@ -0,0 +1,14 @@
+# Condition is about skipping a particular task to run or not.
+- name: Condition Demo
+  hosts: localhost
+  connection: local
+  tasks:
+    - name: Print Vegetable Name
+      ansible.builtin.debug:
+        msg: Vegetable Names - Carrot
+      when: vegetables == "yes"
+
+    - name: Print Fruit Name
+      ansible.builtin.debug:
+        msg: Fruit Names - Orange
+      when: fruits == "yes"
